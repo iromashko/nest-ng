909761b826314d52e66e3fa9ab0f70455a9cc440
iromashko <iromashko@me.com>
2021-01-18 07:14:07 +0300

    part 5

--- nest-admin/src/user/user.module.ts
+++ nest-admin/src/user/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './models/user.entity';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  imports: [TypeOrmModule.forFeature([User])]
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService]
 })
 export class UserModule {}
