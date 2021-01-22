e054b7de5e39e458c97351bb7e5e61f306edb0e9
iromashko <iromashko@me.com>
2021-01-22 13:44:16 +0300

    part 21  Add Orders module

---
+++ nest-admin/src/order/order.controller.ts
@@ -0,0 +1,15 @@
+import { ClassSerializerInterceptor, Controller, Get, Query, UseGuards, UseInterceptors } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { OrderService } from './order.service';
+
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(AuthGuard)
+@Controller()
+export class OrderController {
+  constructor(public orderService: OrderService) {}
+
+  @Get('orders')
+  async all(@Query('page') page = 1) {
+    return this.orderService.paginate(page, ['order_items']);
+  }
+}
---
+++ nest-admin/src/order/order.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/common/abstract.service';
+import { IPaginateResponse } from 'src/common/paginate-response.interface';
+import { Repository } from 'typeorm';
+import { Order } from './order.entity';
+
+@Injectable()
+export class OrderService extends AbstractService {
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+  ) {
+    super(orderRepository);
+  }
+
+  async paginate(page = 1, relations: any[] = []): Promise<IPaginateResponse> {
+    const { data, meta } = await super.paginate(page, relations);
+
+    return {
+      data: data.map((order: Order) => ({
+        id: order.id,
+        name: order.name,
+        email: order.email,
+        total: order.total,
+        created_at: order.created_at,
+        order_items: order.order_items
+      })),
+      meta,
+    };
+  }
+}
