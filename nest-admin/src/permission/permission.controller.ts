811bd00cd6fa6e2577a422a5a18edc0558fc985d
iromashko <iromashko@me.com>
2021-01-22 17:17:45 +0300

    part 24 Permission module

--- nest-admin/src/permission/permission.controller.ts
+++ nest-admin/src/permission/permission.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { HasPermission } from './has-permission.decorator';
 import { Permission } from './permission.entity';
 import { PermissionService } from './permission.service';
 
+@UseGuards(AuthGuard)
 @Controller('permissions')
 export class PermissionController {
   constructor(private permissionService: PermissionService) {}
 
   @Get()
+  @HasPermission('view_permissions')
   async all(): Promise<Permission[]> {
     return this.permissionService.all()
   }
