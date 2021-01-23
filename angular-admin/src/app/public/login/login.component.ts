e1b21ebceca232576dfc724166654a2c8d7d15cd
iromashko <iromashko@me.com>
2021-01-23 04:27:04 +0300

    part 4 Login feature

--- angular-admin/src/app/public/login/login.component.ts
+++ angular-admin/src/app/public/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss', './../public.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      email: '',
+      password: '',
+    });
   }
 
+  public submit(): void {
+    this.http
+      .post(`${environment.api}/login`, this.form.getRawValue(), {
+        withCredentials: true,
+      })
+      .subscribe(() => this.router.navigate(['/']));
+  }
 }
