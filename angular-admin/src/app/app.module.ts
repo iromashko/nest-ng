3b5c8157ffae219a474c3da9f36f1e4706cb89d0
iromashko <iromashko@me.com>
2021-01-23 03:17:38 +0300

    part 4 Register user

--- angular-admin/src/app/app.module.ts
+++ angular-admin/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicModule } from './public/public.module';
 import { SecureModule } from './secure/secure.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SecureModule, PublicModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SecureModule,
+    PublicModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- angular-admin/src/app/public/register/register.component.ts
+++ angular-admin/src/app/public/register/register.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-register',
@@ -12,17 +15,21 @@ export class RegisterComponent implements OnInit {
   password: '';
   passwordConfirm: '';
 
-  constructor() {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {}
 
   public submit(): void {
-    console.log({
-      first_name: this.firstName,
-      last_name: this.lastName,
-      email: this.email,
-      password: this.password,
-      password_confirm: this.passwordConfirm,
-    });
+    this.http
+      .post(`${environment.api}/register`, {
+        first_name: this.firstName,
+        last_name: this.lastName,
+        email: this.email,
+        password: this.password,
+        password_confirm: this.passwordConfirm,
+      })
+      .subscribe(() => {
+        this.router.navigate(['/login']);
+      });
   }
 }
